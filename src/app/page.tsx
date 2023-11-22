b80042e44e2ee4219825facb5861ff9bd5eccbad
Joao Victor Costa <victor_ngl2@hotmail.com>
2023-11-22 14:07:07 -0300

    [UPDATE] Update hub next config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Next.js</h1>
-      <Image src="/hub/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
     </div>
   )
 }
